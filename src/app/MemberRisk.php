7eb2ac49b660e7b71fcce12c9d6a878637388433
Mav Peri <mp@h2labs.co.uk>
2017-12-25 23:08:01 +1100

    release

--- src/app/MemberRisk.php
+++ src/app/MemberRisk.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class MemberRisk extends Model
 {
-    protected $table = 'MemberRisk';
+    protected $table = 'MemberRisk'; 
     protected $connection = "crutils";
     protected $fillable = [];
 }
